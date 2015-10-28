815e9d38df43a9b462bedb70a5fa33ff29eaf6fc
pablocastilla <pablo.castilla@gmail.com>
2015-10-28 11:48:41 +0100

    ups, missing file

---
+++ InvokeSagaExceptionInNoSagaEndpoint/CommandXHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NServiceBus;
+
+namespace InvokeSagaExceptionInNoSagaEndpoint
+{
+    public class CommandXHandler : IHandleMessages<CommandX>
+    {
+        public void Handle(CommandX message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
