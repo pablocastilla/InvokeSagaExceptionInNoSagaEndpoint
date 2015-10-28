71cd1cacbab4d33246de945e609c688269060035
pablocastilla <pablo.castilla@gmail.com>
2015-10-28 11:47:45 +0100

    Final code

---
+++ InvokeSagaExceptionInNoSagaEndpoint/ConnectionOpenerStep.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NServiceBus.Pipeline;
+using NServiceBus.Pipeline.Contexts;
+
+namespace InvokeSagaExceptionInNoSagaEndpoint
+{
+    public class ConnectionOpenerBehavior : IBehavior<IncomingContext>
+    {        
+
+        public void Invoke(IncomingContext context, Action next)
+        {
+        
+        }
+
+    }
+
+    class ConnectionOpenerStep : RegisterStep
+    {
+        public ConnectionOpenerStep()
+            : base("ConnectionOpenerStep", typeof(ConnectionOpenerBehavior), "opens the connection to the database before the handler is processed")
+        {
+            
+            InsertBefore(WellKnownStep.InvokeSaga);
+            //InsertBefore(WellKnownStep.InvokeHandlers); <-- this and others step works, Saga doesn't
+        }
+    }
+}
